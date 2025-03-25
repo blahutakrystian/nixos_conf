7c0891070ddfc2968e320d2ddf0b26c9bdf008ae
blahutakrystian <blahutakrystian@gmail.com>
2025-03-25 23:22:09 +0100

    add aliases

---
+++ aliases.nix
@@ -0,0 +1,8 @@
+# aliases.nix
+{
+  environment.shellAliases = {
+    rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
+    upgrade = "sudo nix flake update /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos";
+    clean-old-builds = "sudo nix-collect-garbage --delete-older-than 3d";
+  };
+}
--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./aliases.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
