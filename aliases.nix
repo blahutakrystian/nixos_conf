2d981820dcf0298082a7866958a512c283f17d97
blahutakrystian <blahutakrystian@gmail.com>
2025-03-25 23:38:56 +0100

    fix aliases(upgrade), set home managment to same version as pkgs

--- aliases.nix
+++ aliases.nix
@@ -2,7 +2,7 @@
 {
   environment.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
-    upgrade = "sudo nix flake update /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos";
+    upgrade = "sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos";
     clean-old-builds = "sudo nix-collect-garbage --delete-older-than 3d";
   };
 }
